d459da61edae13b1efacab1827012153416d6c57
Dip Chowdhury <dipcdy6709@gmail.com>
2026-09-13 20:15:28 +0600

    first and second lecture

---
+++ Js-01-DataTypes/script.js
@@ -0,0 +1,28 @@
+console.log(`Hi
+    ni`);
+
+
+
+    
+console.log(`hiii`);
+
+let a = 14;
+let b = 3;
+let c = 6
+
+console.log(a>=c);
+const timmyAge = 18
+if (timmyAge >= 16) {
+  console.log("Timmy is old enough to drive.");
+}else{
+  console.log("Timmy is not old enough to drive.")
+}
+//JavaScript Display Possibilities:
+
+// window.alert("string")
+// window.print()
+// document.write("I am procodrr")
+console.log(!!undefined);
+
+
+console.log(a << 1);
---
+++ Js-02-Variables/script.js
@@ -0,0 +1,22 @@
+
+var firstName = "Dip"
+var lastName = "chowdhury"
+
+let userIntro = "My name is Dip Chowdhury"
+let age= 25
+
+let digit = 10
+let a  = 'dip'
+
+let username
+ username= "is defined"
+  
+ 
+ 
+var খুশি = true
+let isHappy = 
+"Hi I am " + 
+a + " " + lastName + " and i am " + age +
+ " years old."
+
+ 
