b6ca1e026243a7ba42e21474333cc3094c5deaaa
Dip Chowdhury <dipcdy6709@gmail.com>
2026-09-21 10:05:28 +0600

    Reference and Object-freeze,seal methods

---
+++ js-13-Reference types(objects)/script.js
@@ -0,0 +1,48 @@
+const myName = "Dip"
+const myName2 = "CHY"
+
+const username1 = ""
+const username2 = ""
+
+const user = {
+  firstName: "Dip",
+
+}
+// const user2 = {
+//     firstName: "Dipto",
+//     lastName: "Chowdhury",
+//     Dip: "Devoloper"
+// }
+// console.log(user2.lastName);
+// console.log(user2["lastName"]);
+// console.log(user2["first"+ "Name"]);
+
+
+const user2 = {
+  
+    firstName: "Dipto",
+    lastName: "Chowdhury",
+
+    address: {
+      city: "Chittagong",
+      PO: "Fatehabad",
+      District: "Chittagong",
+      PostalCode: 4335,
+      moreDetails: {
+        population: 200,
+        area: "23 sq km"
+      }
+    }
+}
+user2["is-Student"] = true;
+
+// const user3 = {
+//   name: "dip",
+//   age: {
+//     minage: 10,
+//     maxage: {
+//       min: 23,
+//       max: 25
+//     }
+//   }
+// }
---
+++ js-14-object-freeze and seal()/script.js
@@ -0,0 +1,33 @@
+// const user = {
+  
+//     firstName: "Dipto",
+//     lastName: "Chowdhury",
+
+//     address: {
+//       city: "Chittagong",
+//       PO: "Fatehabad",
+//       District: "Chittagong",
+//       PostalCode: 4335,
+//        moreDetails: {
+//          population: 200,
+//          area: "23 sq km"
+//        },
+       
+//     },
+//     age: 20,
+//     isGraduate: false,
+// }
+// username = "dip"
+
+// Object.seal(user)
+// Object.freeze()
+// Object.isSealed() // Return value true or false
+// console.log(Object.isFrozen(user));// Return value true or false
+
+let x =1;
+
+
+
+// IMPORTANT...............!!!!
+//IN OBJECT.FREEZE() IT ONLY FREEZES THE PRIMITIVE VALUE IF THERE IS A
+//NESTED OBJECT OR AN ARRAY INSIDE A OBJECT IT CAN BE MUTED,DELETED AND ADDED.
