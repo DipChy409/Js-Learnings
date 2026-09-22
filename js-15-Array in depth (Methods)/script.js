ce9cf6c79208ccc06c4bd82cc3a7d3ce34298b14
Dip Chowdhury <dipcdy6709@gmail.com>
2026-09-22 07:35:43 +0600

    Array Methods and arrays in Array

---
+++ js-15-Array in depth (Methods)/script.js
@@ -0,0 +1,47 @@
+// const username = "Dip Chowdhury"
+
+// const fruitsCollection = ["Apple","Banana","Grapes","Mango", 25]
+
+// // const rainbowColor = ["Violet", "Blue", "Indigo", "Green", "Yellow", "Black", "Red"]
+
+// const user = ["Messi", "Ronaldo"]
+
+
+// user.push("Backham", "Ronaldhino") // add array items
+// user.pop()// delete items of arrays from last
+// user[0] = "Lionel Messi"// change Messi to Lionel Messi used to add or changing the array items 
+
+
+
+// const fruitsCollection = ["Apple","Banana","Grapes","Mango", 25]
+// fruitsCollection.shift()
+// fruitsCollection.unshift("Apple")
+// console.log(fruitsCollection);
+
+
+// const animals = ["Lion", "Hyenas" ]
+// const animals2 = ["panda", "Tiger"]
+// const animals3 = ["Horse", "Giraffe"]
+// const animals4 = ["Ostrich", "Pig"]
+
+// const mergeAnimals = animals.concat(animals2, animals3, animals4)
+// console.log(mergeAnimals);  // used to add arrays
+
+// const fruitsCollection = ["Apple","Banana","Grapes","Mango", 25]
+// fruitsCollection.indexOf(25)  =4      //show its index or position as indexed
+
+// let animals = ["Lion", "Hyenas" , true]
+// animals.includes("Lion")  //true 
+
+
+// const Number = [0,1,2,3,4,5]
+// Number.reverse()      //reverse the Number array
+
+// let animals = ["lion", "Hyenas" , "Rat", "Dog", "dog", "Lion"]
+// animals.sort()    sort by 1st word or number
+
+// let animals = ["Lion", "Hyenas" , "Rat", "Dog", ]
+// animals.slice()  // slice the array
+
+// let animals = ["Lion", "Hyenas" , "Rat", "Dog", ]
+// animals.splice(1,2, "Giraffe", "panda", 1, true) // delete and add new type
