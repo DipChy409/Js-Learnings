b3ac6a6046fceed0b0c658228c5824a92f0b7980
Dip Chowdhury <dipcdy6709@gmail.com>
2026-09-24 18:12:49 +0600

    while Loop and basic functions

---
+++ js-18-While Loop/script.js
@@ -0,0 +1,87 @@
+//WHILE LOOP............
+
+// console.log("Program Started");
+// debugger
+// let i = 1
+
+// while( i <= 100){
+//     console.log(i);
+//     i++
+// }
+
+// let friends = ["Rahul", "Akash", "Dip", "Dipto", "Monil", "Gourav", "pintu", "Rinku", "Rahul", "Akash", "Dip", "Dipto"]
+//  let i = 0
+// while(i<friends.length){
+//     console.log(`${i+1}:`, `${friends[i]}: ProCodrr`)
+//     i++
+
+// }
+
+// console.log("Program Ended");
+
+// let i = 10
+// while (i <= 30) {
+//   if (i % 2 === 1) {
+//     console.log(i)
+//   }
+//   i++
+// }
+
+
+
+//FOR LOOP.............
+
+// console.log("Program Started")
+debugger
+
+for(const i = 0; i< 10; i++ ){
+    console.log(i+1);
+}
+
+// debugger
+// const arr= ["Rahul", "Akash", "Dip", "Dipto"]
+// for(let i =0; i< arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// console.log("Programm Ended");
+
+
+            // Some Question and its answer----
+
+// for(let i=2; i<=50;i++){
+//     if(i%2 === 0){
+//         console.log(i);
+//     }
+// }              /// THis answer Q is : print even number between 1 to 50
+
+
+// debugger
+
+// const number = [3, 8, 15, 22, 7, 40, 13, 6];
+
+// for (let i = 0; i < number.length; i++) {
+//     let a = number[i]; // ✅ update a every iteration
+//     if (a % 2 === 0) {
+//         console.log(`Even: ${a}`);
+//     } else {
+//         console.log(`Odd: ${a}`);
+//     }
+// }   /// This anser Question is :Use a for loop to go through the array and print:
+
+// "Even: 8" if the number is even
+// "Odd: 3" if the number is odd
+
+
+
+// Do-While Loop...............
+// debugger
+// console.log("Program started");
+
+// let i = 0;
+// do{
+//     console.log(i);
+//     i++
+// }while(i<12)
+
+//     console.log("Programm Ended");
---
+++ js-19-function & Return value/script.js
@@ -0,0 +1,51 @@
+
+//Function Defination....
+// function introduceMe(username, Role, age) {
+//     console.log(Role);
+  
+// console.log(`Hallo Freunde, Ich heise ${username}`);
+// console.log(`I am a ${Role}`);
+// console.log(`He is ${age} Years old.`);
+// return true;
+
+// // console.log(typeof username);
+// // console.log(typeof Role);
+// // console.log(typeof age);
+
+// }
+
+
+
+// //Function Call
+// introduceMe("Dipto", true, 21)
+// introduceMe("Rana", " Mech Engr", 30)
+// introduceMe("Collins", "Civil Engr", 20)
+
+
+
+
+
+
+////Return Keyword....
+// function  addTwoNumbers(){
+//     return 5+6;
+// }
+
+//  const result =addTwoNumbers()
+//     debugger
+
+// console.log("dip");
+
+// function sumNumber(a, b){
+//     return a+b
+    
+    
+// }
+
+  
+// const result=  sumNumber(8, 6)
+// sumNumber(sumNumber(sumNumber(3,5),sumNumber(7,8)), sumNumber(sumNumber(9,5),3))
+// console.log(result);
+
+
+
