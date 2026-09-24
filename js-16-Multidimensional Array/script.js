831c6abb6c1e1e07a96d62376902244dfca42c76
Dip Chowdhury <dipcdy6709@gmail.com>
2026-09-24 18:21:28 +0600

    some minor changes in 16-17

---
+++ js-16-Multidimensional Array/script.js
@@ -0,0 +1,25 @@
+// const nameandNumber = [
+//   ["Dipo", 96],
+//   ["Rahul", 92],
+//   ["Kumar", 97],
+// ]
+
+// const ticTactoe = [
+//   ["X", null, 0],
+//   [null, null, null],
+//   [null, 0, "X"],
+// ]
+
+
+// let arr =[[["sports",["Reality-games", ["virtual-games"]]],"online-games"],["Games"]] //array inside array
+
+
+// let objName = {name: "DIP" ,age:{}, birthyear: 2005, arr: [1,0,["Array", {name:"Dipto"}]]}
+
+// let obj2 = objName.arr[2][0].name
+// console.log(obj2);
+let user1 = { name: 'Alice',name2: "Dipo", age: 20 };
+
+user1 = { name: 'Bob' };
+let user2 = user1;
+
