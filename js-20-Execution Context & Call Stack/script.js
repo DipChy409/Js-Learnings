b94cf1ba10de313406169cd0ce7c38d36ab5239a
Dip Chowdhury <dipcdy6709@gmail.com>
2026-09-25 19:51:04 +0600

    ExecutionContext and callstack

---
+++ js-20-Execution Context & Call Stack/script.js
@@ -0,0 +1,89 @@
+
+ //My Code...............
+
+// let username = "DIP";
+// const userAge = 21;
+// function keyword(a){
+//     return a+1
+// }
+
+// //Memory Creation Phase.......
+// let username = undefined;// let → hoisted BUT in TEMPORAL DEAD ZONE
+// const userAge =undefined;
+// function keyword(a){return a+1}
+
+// //Code Execution Phase........
+// let username = "DIP"
+// const userAge =21;
+// function (Unchanged cause it hasnot make undefined in creation instead it has store full body or code inside the function)
+
+
+// sayHi()
+
+// const username = 'Anurag'
+// const userAge = 25
+
+// function sayHi() {
+//     const a = 14
+//     const b = 12
+//     add(7, 9)
+// }
+
+// function add(x, y) {
+//     kuchhBhi()
+//     return x + y
+// }
+
+// function kuchhBhi() {
+//     console.log('Kuchh bhi');
+// }
+
+//       Call Stack...
+
+
+// sayHi()
+
+// function sayHi(){
+//     const a = 10;
+//     var b = 12;
+//     console.log(a,b);
+// add(2,3)
+
+// }
+// function add(x,y){
+//     subsTract(3,2)
+//     return x+y;
+// }
+
+// function subsTract(a,b){
+//     return a-b;
+// }
+// console.log("Program Ended");
+
+
+
+
+//Stack Overflowed.....
+// debugger
+// function introduceMe(){
+// console.log("HI Iam Dip");
+// introduceMe()
+// }
+// introduceMe()
+
+// debugger
+// function countdown(n) {
+//     if (n === 0) return; // 🛑 STOP here!
+//     console.log(n);
+//     countdown(n - 1);
+// }
+
+// countdown(5);
+
+// function num(a){
+//     if(a>5) return;
+//     console.log(a);
+//     num(a+1)
+
+// }
+// num(1)
